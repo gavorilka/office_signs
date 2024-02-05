fd102265406f8661f3843241096dcf971c24b02e
gavorilka <59140477+gavorilka@users.noreply.github.com>
2024-02-05 12:37:51 +0300

    test timer

--- src/js/renderFunction.js
+++ src/js/renderFunction.js
@@ -4,3 +4,9 @@ const weekDay = ['Пн','Вт','Ср','Чт','Пт','Cб','Вс']
 const url = new URL(window.location)
 const urlCabinet = url.searchParams.get('cabinet')
 document.querySelector('.cabinet').append(`Кабинет ${urlCabinet}`)
+const time = () => {
+    const timeBlock = document.querySelector('.time')
+    const date = new Date()
+    timeBlock.textContent =`Урок №4 ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+}
+setInterval(time, 1000, );
